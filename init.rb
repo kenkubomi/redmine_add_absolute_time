8b8f79320f836fd05c942ed39b1b749a4418d18f
Ken Kubomi <k_kubomi@tairapromote.co.jp>
2024-02-02 18:07:57 +0900

    update

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_add_absolute_time do
   name 'Redmine Add Absolute Time plugin'
   author 'Ken Kubomi'
   description 'This is a plugin that adds an absolute time notation to the relative time notation.'
-  version '0.0.1'
+  version '0.0.2'
   url 'https://github.com/kenkubomi/redmine_add_absolute_time'
   author_url 'https://qiita.com/kenkubomi'
 end
--- lib/application_helper_patch.rb
+++ lib/application_helper_patch.rb
@@ -1,10 +1,15 @@
 module ApplicationHelperPatch
   def time_tag(time)
+    return if time.nil?
+
     text = distance_of_time_in_words(Time.now, time)
+    text = "[#{format_time(time)}] #{text}"
     if @project
-      " " + link_to("[#{format_time(time)}] #{text}", project_activity_path(@project, :from => User.current.time_to_date(time)), :title => format_time(time))
+      link_to(text,
+              project_activity_path(@project, :from => User.current.time_to_date(time)),
+              :title => format_time(time))
     else
-      " " + content_tag('abbr', "[#{format_time(time)}] #{text}", :title => format_time(time))
+      content_tag('abbr', text, :title => format_time(time))
     end
   end
 end
